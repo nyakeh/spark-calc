c2f852d04463b379cd69d4b95865cea37538b06b
Nyakeh Rogers <nyakeh@users.noreply.github.com>
2023-10-21 15:39:37 +0100

    Input form with basic initial question fields.

--- src/Coast.tsx
+++ src/Coast.tsx
@@ -1,15 +1,6 @@
 import './App.css';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Filler,
-  Legend,
-} from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale,PointElement, LineElement, Title, Tooltip, Filler, Legend } from 'chart.js';
+import { useState } from 'react';
 import { Line } from 'react-chartjs-2';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler, Legend);
@@ -36,18 +27,45 @@ export const data = {
       fill: true,
       label: 'Net worth',
       data: [100,4000,12000,20000,29000,36000,44000,53000,65000,71000,80000,96000],
-      borderColor: 'rgb(53, 162, 235)',
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      borderColor: 'rgb(145, 163, 121)',
+      backgroundColor: 'rgba(145, 163, 121, 0.3)',
     },
   ],
 };
 
 function Coast() {
+  const [investedAssets, setInvestedAssets] = useState(0);
+  const [age, setAge] = useState(30);
+  const [monthlyContribution, setMonthlyContribution] = useState(500);
+
+  const reCalculate = (event: any) => {
+    event.preventDefault();
+    alert(`investedAssets: ${investedAssets}`)
+  }
+
   return (
-    <section className='App-main'>
-      <h2>Coast FI &#128293; Calculator</h2>
-      <Line options={options} data={data} />
-    </section>
+    <div>
+      <section className='App-main'>
+        <h2>Coast FI &#128293; Calculator</h2>
+        <Line options={options} data={data} />
+        <p>You're on track to have <span>£328,167</span> at retirement. If you take <span>£26,000</span> per year, this will last until age <span>90</span>.</p>
+      </section>
+
+      <section className='App-main'>
+        <form onSubmit={reCalculate}>
+          <label>Invested Assets: 
+            <input type='number' value={investedAssets} onChange={e => setInvestedAssets(Number(e.target.value))} />
+          </label>
+          <label>Age: 
+            <input type='number' value={age} onChange={e => setAge(Number(e.target.value))} />
+          </label>
+          <label>Monthly Contribution: 
+            <input type='number' value={monthlyContribution} onChange={e => setMonthlyContribution(Number(e.target.value))} />
+          </label>
+          <input type="submit" />
+        </form>
+      </section>
+    </div>
   );
 }
 
