cdca5796280050cc70b72a4d0924684566237086
Nyakeh Rogers <nyakeh@users.noreply.github.com>
2023-10-23 22:48:36 +0100

    Adding complexity around tax calculations to include UK tax bands

--- src/CalculateSaving.tsx
+++ src/CalculateSaving.tsx
@@ -1,11 +1,48 @@
 import "./App.css";
 
+function CalculateIncomeTax(salary: number) {
+  if(salary <= 12570){
+    return 0;
+  }
+  let remainingSalary = salary - 12570;
+  if(remainingSalary <= 37699) {
+    return remainingSalary * 0.2;
+  }
+  remainingSalary = remainingSalary - 37699; 
+  let twentyPercentBand = 37699 * 0.2;
+  if(remainingSalary <= 87439) {
+    return twentyPercentBand + (remainingSalary * 0.4)
+  }
+  remainingSalary = remainingSalary - 87439;
+  let fortyPercentBand = 87439 * 0.4;
+  return twentyPercentBand + fortyPercentBand + (remainingSalary * 0.45);
+}
+
+function CalculateNationalInsurance(salary: number) {
+  if(salary <= 12570){
+    return 0;
+  }
+  let remainingSalary = salary - 12570;
+  if(remainingSalary <= 37699) {
+    return remainingSalary * 0.12;
+  }
+  remainingSalary = remainingSalary - 37699;
+  let twentyPercentBand = 37699 * 0.12;
+  return twentyPercentBand + (remainingSalary * 0.02)
+}
+
 function CalculateSaving(props: { salary: number; pensionContribution: number; monthlySavings: number }) {
 	let pensionCash = props.salary * (props.pensionContribution / 100);
 	let annualSaving = props.monthlySavings * 12;
 	let totalSaved = pensionCash + annualSaving;
 	let grossSavedPercentage = (totalSaved / props.salary) * 100;
-	let tax = props.salary * 0.2;
+  let taxableIncome = props.salary - pensionCash
+  let incomeTax = CalculateIncomeTax(taxableIncome);
+  let nationalInsurance = CalculateNationalInsurance(taxableIncome);
+  console.log("incomeTax: "+ incomeTax);
+  console.log("nationalInsurance: "+ nationalInsurance);
+	let tax = incomeTax + nationalInsurance;
+
 	let netSavedPercentage = (totalSaved / (props.salary - tax)) * 100;
 
 	return (
